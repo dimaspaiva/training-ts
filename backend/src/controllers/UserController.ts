f4e942d3b87770389615404b7df979075c1d73be
dimaspaiva <dimasalpaiva@gmail.com>
2020-03-10 09:08:45 -0300

    implements fake mail service

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,9 +1,28 @@
 import { Request, Response } from 'express';
 
+import EmailService from '../services/EmailService';
+
 const users = [{ name: 'Dimas Paiva', email: 'dimasalpaiva@gmail.com' }];
 
 export default {
   async index(req: Request, res: Response) {
     return res.json(users);
   },
+
+  async create(req: Request, res: Response) {
+    const emailService = new EmailService();
+
+    emailService.sendMail({
+      to: {
+        email: 'dimasalpaiva@gmail.com',
+        name: 'Dimas Paiva'
+      },
+      message: {
+        subject: 'Welcome to system',
+        body: 'Welcome'
+      }
+    });
+
+    res.send();
+  }
 };
